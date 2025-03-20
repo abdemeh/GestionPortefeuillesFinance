096d48c082b61b5440fabfebfd1566f6b35e62ec
mohamed_lamine_koné <kmohamedlamine949@gmail.com>
2025-03-20 13:28:43 +0100

    remise des fichiers supprimés et modif de la page blog details

--- frontend/src/components/DashBoardLayerFour.jsx
+++ frontend/src/components/DashBoardLayerFour.jsx
@@ -1,39 +1,33 @@
-import React from 'react'
-import UnitCountThree from './child/UnitCountThree'
-import CoinAnalyticsOne from './child/CoinAnalyticsOne'
-import CryptoMostPopular from './child/CryptoMostPopular'
-import CryptoTrendingNow from './child/CryptoTrendingNow'
-
-import MyOrdersOne from './child/MyOrdersOne'
-import RecentTransactionOne from './child/RecentTransactionOne'
-import MyCardsOne from './child/MyCardsOne'
-import TotalBalanceOne from './child/TotalBalanceOne'
-import UserActivatesOne from './child/UserActivatesOne'
+import React from 'react';
+import CryptoMostPopular from './child/CryptoMostPopular';
+import CryptoTrendingNow from './child/CryptoTrendingNow';
+import NewsPanel from './child/NewsPanel';
+import UnitCountThree from './child/UnitCountThree';
 
 const DashBoardLayerFour = () => {
   return (
-    <>
       <section>
         <div className="row gy-4 mt-4">
           {/* Crypto Home Widgets Start */}
           <div className="col-xxl-8">
             <UnitCountThree />
-            <br/>
-            <div className="row gy-4">
-
+            <div className="row gy-4 mt-4">
               <CryptoMostPopular />
-
-
+            </div>
+            <div className="row gy-4 mt-4">
+              <CryptoTrendingNow />
             </div>
           </div>
-
           {/* Crypto Home Widgets End */}
 
-
+          <div className="col-xxl-4">
+            <div className="row gy-4">
+              <NewsPanel />
+            </div>
+          </div>
         </div>
       </section>
-    </>
-  )
-}
+  );
+};
 
-export default DashBoardLayerFour
+export default DashBoardLayerFour;
